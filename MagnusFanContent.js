5bb298e1f20e53c6b4d65c48c119b7505f46246e
ShanyeEast <Shane.hogan37@mail.dcu.ie>
2024-12-20 13:03:55 +0000

    Delete MagnusFanContent.js

--- MagnusFanContent.js
+++
@@ -1,9 +0,0 @@
-function toggleSpoiler(event, id) {
-    event.preventDefault();
-    var spoilerContent = document.getElementById(id);
-    if (spoilerContent.style.display === 'none' || spoilerContent.style.display === '') {
-        spoilerContent.style.display = 'block';
-    } else {
-        spoilerContent.style.display = 'none';
-    }
-}
